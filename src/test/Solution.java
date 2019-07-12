e1a062c9b24f991aaefd3127dfb7ca36330e97d7
dejunqi2008 <dejunqi2008@gmail.com>
2019-07-11 22:16:56 -0700

    c

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -1,14 +1,22 @@
 package test;
 
-
-
 import java.util.*;
 
 public class Solution {
-    public void test() {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        map.put(1, 1);
-//        map.lastKey()
-    }
+    public int minMeetingRooms(int[][] intervals) {
 
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int[] e : intervals) {
+            map.put(e[0], map.getOrDefault(e[0], 0) + 1);
+            map.put(e[1], map.getOrDefault(e[1], 0) - 1);
+        }
+
+        int res = 0;
+        int cnt = 0;
+        for (Integer key : map.keySet()) {
+            cnt += map.get(key);
+            res = Math.max(res, cnt);
+        }
+        return res;
+    }
 }
