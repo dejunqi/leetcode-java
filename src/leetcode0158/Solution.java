40660115d8bd76bc9a67da2eafe606864269a3fc
dejunqi2008 <dejunqi2008@gmail.com>
2019-12-22 22:54:52 +0800

    mend

--- src/leetcode0158/Solution.java
+++ src/leetcode0158/Solution.java
@@ -7,30 +7,36 @@ public class Solution extends Reader4 {
      * @param n   Number of characters to read
      * @return    The number of actual characters read
      */
+
     private Queue<Character> queue = new LinkedList<>();
+
     public int read(char[] buf, int n) {
-        int time = n / 4;
-        if (n % 4 > 0) {
-            time += 1;
+        int idx = 0;
+        while (!queue.isEmpty() && idx < n) {
+            buf[idx++] = queue.poll();
         }
 
-        int ptr = 0;
+
+        int time = (n - idx) / 4;
+        if ( (n - idx) % 4 != 0) {
+            time++;
+        }
 
         for (int i = 0; i < time; i++) {
             char[] tmp = new char[4];
             int num = read4(tmp);
             for (int j = 0; j < num; j++) {
-                queue.offer(tmp[j]);
+                if (idx < n) {
+                    buf[idx++] = tmp[j];
+                } else {
+                    queue.offer(tmp[j]);
+                }
             }
-
-            if (queue.isEmpty() || ptr == n) break;
-
-            while (ptr < n && !queue.isEmpty()) {
-                buf[ptr] = queue.poll();
-                ptr += 1;
+            if (num < 4) {
+                break;
             }
         }
 
-        return ptr;
+        return idx;
     }
 }
---
+++ src/leetcode0437/Solution.java
@@ -0,0 +1,30 @@
+package leetcode0437;
+import utils.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int[] res = {0};
+        helper(root, sum, map, res, 0);
+        return res[0];
+    }
+
+    private void helper(TreeNode node, int target, Map<Integer,
+            Integer> map, int[] res, int cursum) {
+        if (node == null) {
+            return;
+        }
+        cursum += node.val;
+        int value = cursum - target;
+        res[0] += map.getOrDefault(value, 0);
+        map.put(cursum, map.getOrDefault(cursum, 0) + 1);
+        helper(node.left, target, map, res, cursum);
+        helper(node.right, target, map, res, cursum);
+        map.put(cursum, map.get(cursum) - 1);
+    }
+}
