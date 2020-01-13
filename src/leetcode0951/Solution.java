16167de64b5a0cee9f9301cd5244313b307eea2e
dejunqi2008 <dejunqi2008@gmail.com>
2020-01-13 11:09:59 +0800

    mend

--- src/leetcode0951/Solution.java
+++ src/leetcode0951/Solution.java
@@ -2,20 +2,24 @@ package leetcode0951;
 import utils.TreeNode;
 
 public class Solution {
-
-
-    public boolean flipEquiv(TreeNode p1, TreeNode p2) {
-        if (p1 == null || p2 == null) {
-            return p1 == p2;
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
         }
 
-        if (p1.val != p2.val) {
+        if (root1 == null || root2 == null) {
             return false;
         }
 
-        boolean case1 = flipEquiv(p1.left, p2.left) && flipEquiv(p1.right, p2.right);
-        boolean case2 = flipEquiv(p1.left, p2.right) && flipEquiv(p1.right, p2.left);
-        return case1 || case2;
-    }
+        if (root1.val != root2.val) {
+            return false;
+        }
 
+        // same tree
+        boolean case1 = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        // symetric case
+        boolean case2 = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+        return case1 || case2;
+
+    }
 }
--- src/utils/practice/Solution.java
+++ src/utils/practice/Solution.java
@@ -1,52 +1,48 @@
 package utils.practice;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+
+
+import utils.TreeNode;
 
 public class Solution {
 
-    private class Item {
-        double ratio;
-        double quality;
-        Item(int w, int q) {
-            ratio = (w * 1.0) / q;
-            quality = q;
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> todelete = new HashSet<>();
+        for (int v : to_delete) {
+            todelete.add(v);
         }
-    }
-
-    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
-        int len = quality.length;
-        Item[] items = new Item[len];
-        for (int i = 0; i < len; i++) {
-            items[i] = new Item(wage[i], quality[i]);
-        }
-
-        Arrays.sort(items, Comparator.comparingDouble(o -> o.ratio));
-        PriorityQueue<Double> queue = new PriorityQueue<>((a, b) -> (int)(b - a));
-        int totalQuality = 0;
-        double res = Double.MAX_VALUE;
-
-        for (Item item : items) {
-            queue.add(item.quality);
-            totalQuality += item.quality;
-            if (queue.size() > K) {
-                totalQuality -= queue.poll();
-            }
-            if (queue.size() == K) {
-                res = Math.min(res, totalQuality * item.ratio);
-            }
-        }
+        int MAX = Integer.MAX_VALUE;
+        TreeNode dummy = new TreeNode(MAX);
+        dummy.left = root;
+        todelete.add(MAX);
+        List<TreeNode> res = new ArrayList<>();
+        helper(dummy, res, todelete);
         return res;
     }
 
-    public void test() {
+    private TreeNode helper(TreeNode node, List<TreeNode> res, Set<Integer> todelete) {
+        if (node == null) {
+            return null;
+        }
 
-        int[] quality = {10,20,5};
-        int[] wage = {70,50,30};
-        int K = 2;
-        System.out.println(mincostToHireWorkers(quality, wage, K));
+        TreeNode left = helper(node.left, res, todelete);
+        TreeNode right = helper(node.right, res, todelete);
+
+        if (todelete.contains(node.val)) {
+            if (left != null) {
+                res.add(left);
+            }
+            if (right != null) {
+                res.add(right);
+            }
+            return null;
+        }
+
+        node.left = left;
+        node.right = right;
+        return node;
 
     }
 }
