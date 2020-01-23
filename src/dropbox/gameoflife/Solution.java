e94aa6fb988dfe336c7ace98107af7962df88115
dejunqi <dejunqi@amazon.com>
2020-01-23 00:57:22 -0800

    mend

--- src/dropbox/gameoflife/Solution.java
+++ src/dropbox/gameoflife/Solution.java
@@ -4,39 +4,55 @@ import java.io.*;
 
 public class Solution {
 
+    int[][] board;
+
     public void gameoflife() {
         int r = 0;
         int c = 0;
         int pagination = 100;
 
-        while (true) {
-            File file = read(r, c, pagination);
-            if (file == null) {
-                r += pagination;
-                c = 0;
-                file = read(r, c, pagination);
-                if (file == null) {
-                    break;
-                }
-            }
-            write("/", file);
-            c += pagination;
-        }
     }
 
 
-    private File read(int startRow, int starCol, int pagination) {
+    private String read(int startRow, int starCol, int pagination) {
 
         // give a a text file with a matrix of m X n
         // where m = startRow + pagination, n = startCol  + pagination
         // Repeatly invoke this function startfrom from startRow = startCol = 0, and pagination = 100
         // 1st call --> 0 ~ 99 row and col
         // 2ned call --> row from (0, 99) col from (100 ~ 199)
-        return new File("/");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("start: " + startRow  + ", " + starCol + ". Size : " + pagination);
+        sb.append("\n");
+
+        for(int i = startRow; i < startRow + pagination; i++) {
+            for (int j = starCol; j < starCol + pagination; j++) {
+                sb.append(board[i][j]);
+                if (j == starCol + pagination - 1) {
+                    sb.append("\n");
+                }
+            }
+        }
+        return sb.toString();
     }
 
     private void write(String path, File file) {
-        // compute and then write the result intto a file
+        // compute and then write the result into a file
+    }
+
+
+    public void test() {
+        int M = 10000;
+        board = new int[M][M];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Math.random() > 0.5 ? 1 : 0;
+            }
+        }
+
+
+
     }
 
 
