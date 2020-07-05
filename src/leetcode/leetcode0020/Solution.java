53e5d39e4f66a9893c8dc616569cc6c4e016d1e4
dejunqi2008 <dejunqi2008@gmail.com>
2020-07-04 21:13:56 -0700

    mend

---
+++ src/leetcode/leetcode0020/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.leetcode0020;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('{', '}');
+        map.put('[', ']');
+        map.put('(', ')');
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || map.get(stack.peek()) != c )  {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
