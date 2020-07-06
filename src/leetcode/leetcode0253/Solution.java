5627d531a4336f5645fbf9c0b33f42a56c010c23
dejunqi <dejunqi2008@amazon.com>
2020-07-05 18:01:19 -0700

    mend

--- src/leetcode/leetcode0253/Solution.java
+++ src/leetcode/leetcode0253/Solution.java
@@ -1,4 +1,4 @@
-package leetcode0253;
+package leetcode.leetcode0253;
 
 import java.util.TreeMap;
 
@@ -8,23 +8,18 @@ public class Solution {
     // O(n) space
     public int minMeetingRooms(int[][] intervals) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int[] e : intervals) {
-            int start = e[0], end = e[1];
-            // O(log(n)) average in time complexity
-            map.put(start, map.getOrDefault(start, 0) + 1);
-            map.put(end, map.getOrDefault(end, 0) - 1);
+        for (int[] interval: intervals) {
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
         }
 
-        /*
-         * scan the ordered map, if come across start, increase seat by 1, if end time, decrease seat number by 1
-         * The maximum number of seat need arrange during the process is the result.
-         */
         int res = 0;
         int cnt = 0;
         for (Integer key : map.keySet()) {
             cnt += map.get(key);
             res = Math.max(res, cnt);
         }
+
         return res;
     }
 }
